1eb6307e255e06258d9bc6052aa3a67abf5d4fae
unknown <boyaoc@ETC-BOYAOC.andrew.ad.cmu.edu>
2015-10-08 21:00:50 -0400

    timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public static bool start = false;   //start
+	public float gameTime = 60f;		//how long our game should be
+	public Text timerText;              //text object
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (start == false) {
+			gameTime -= Time.deltaTime;
+			timerText.text = "Time Left : " + gameTime;
+			if (gameTime == 0){
+				//call GameManager
+			}
+		}
+	}
+}
