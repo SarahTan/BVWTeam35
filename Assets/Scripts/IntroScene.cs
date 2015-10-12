1f7c0ec42e44edfc15a103b9f5994d9a1102fa5c
boyao <chuby9208@hotmail.com>
2015-10-12 19:24:18 -0400

    added sound , start scene and intro scene

---
+++ Assets/Scripts/IntroScene.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroScene : MonoBehaviour {
+    public Animator CameraMove;
+    public AudioSource IntroMusic;
+    bool NextScene = false;
+	// Use this for initialization
+	void Start () {
+        IntroMusic.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SceneControll();
+    }
+
+    void SceneControll() {
+        Debug.Log("nextmove :" + StartScene.nextMove + " nextScene :" + NextScene);
+        if (Input.GetKeyUp(KeyCode.S)) {
+            StartScene.nextMove = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) && StartScene.nextMove == true && NextScene == false)
+        {
+            CameraMove.SetFloat("CameraMove", 1f);
+            StartScene.nextMove = false;
+            NextScene = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S) && StartScene.nextMove == true && NextScene == true)
+        {
+            Debug.Log("goto level2");
+            Application.LoadLevel(2);
+        }
+
+    }
+
+
+}
